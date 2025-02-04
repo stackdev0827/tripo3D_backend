60d84875bb96c1eff6f4ee1b0177eecc54375cef
stackdev0827 <dreamweaver0827@gmail.com>
2025-02-04 09:45:59 -0500

    database_update

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/test', {
+    await mongoose.connect('mongodb+srv://god880331:2M6mGe8dY9k4S9bk@cluster0.0pftv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
@@ -12,5 +12,6 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
 
 module.exports = connectDB; 
